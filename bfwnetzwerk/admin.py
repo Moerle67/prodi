29f5598822da3e5d2473f7a5b17d2a5a7903b675
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-03-01 16:44:58 +0100

    Anzeigen angepasst

--- bfwnetzwerk/admin.py
+++ bfwnetzwerk/admin.py
@@ -4,13 +4,22 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Fachrichtung)
-admin.site.register(Kontakt)
+@admin.register(Kontakt)
+class KontaktAdmin(admin.ModelAdmin):
+   list_display = ['name', 'mail', 'telefon']
 admin.site.register(Dokument)
-admin.site.register(Organisation)
-admin.site.register(Kostentraeger)
+@admin.register(Organisation)
+class OrganisationAdmin(admin.ModelAdmin):
+   list_display = ['bezeichnung', 'partner']
+   @admin.display()
+   def partner(self, obj):
+      return f"{obj.ansprechpartner.name} -- {obj.ansprechpartner.mail} -- Telefon: {obj.ansprechpartner.telefon}"
+
+   admin.site.register(Kostentraeger)
+
 @admin.register(Reha)
 class RehaAdmin(admin.ModelAdmin):
-   list_display = ['massnahmentitel', 'fachrichtung', 'organisation', 'verantwortlicher', ]
+   list_display = ['massnahmentitel', 'fachrichtung', 'organisation', 'verantwortlicher', 'status']
    search_fields = ['massnahmentitel', 'schlagrichtung', 'schlagwort']
-   list_filter = ['schlagwort','kostentraeger', 'organisation', 'dauer', 'verantwortlicher']
+   list_filter = ['schlagwort','kostentraeger', 'organisation', 'dauer', 'verantwortlicher','status']
    admin.site.register(Schlagwort)
