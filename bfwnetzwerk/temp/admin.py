3cdee86bad9e3272f67cef91abaf061ce47ece08
Ingo Mörl <sag@ich.net>
2023-03-12 15:12:37 +0100

    Fehlersuche

---
+++ bfwnetzwerk/temp/admin.py
@@ -0,0 +1,43 @@
+
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Fachrichtung)
+admin.site.register(Dokument)
+admin.site.register(Kostentraeger)
+@admin.register(Vorschlag)
+class VorschlagAdmin(admin.ModelAdmin):
+    list_display = ['vorschlag', 'user', 'vorschlagdatum']
+    list_filter = ['user']
+    def save_model(self, request, obj, form, change):
+       if not obj.user:
+          obj.user = request.user
+       obj.save()
+@admin.register(Vorschlagbearbeitung)
+class VorschlagbearbeitungAdmin(admin.ModelAdmin):
+    list_display = ['vorschlag', 'anmerkung','user', 'erledigt']
+    list_filter = ['user','vorschlag']
+
+    def save_model(self, request, obj, form, change):
+       obj = Vorschlagbearbeitung(obj)
+       if not obj.user:
+          obj.user = request.user    
+       obj.save()
+@admin.register(Kontakt)
+class KontaktAdmin(admin.ModelAdmin):
+   list_display = ['name', 'mail', 'telefon']
+
+@admin.register(Organisation)
+class OrganisationAdmin(admin.ModelAdmin):
+   list_display = ['bezeichnung', 'partner']
+   @admin.display()
+   def partner(self, obj):
+      return f"{obj.ansprechpartner.name} -- {obj.ansprechpartner.mail} -- Telefon: {obj.ansprechpartner.telefon}"
+      
+@admin.register(Reha)
+class RehaAdmin(admin.ModelAdmin):
+   list_display = ['massnahmentitel', 'fachrichtung', 'organisation', 'verantwortlicher', 'status']
+   search_fields = ['massnahmentitel', 'schlagrichtung', 'schlagwort']
+   list_filter = ['schlagwort','kostentraeger', 'organisation', 'dauer', 'verantwortlicher','status', 'organisation']
+   admin.site.register(Schlagwort)
---
+++ bfwnetzwerk/temp/models.py
@@ -0,0 +1,107 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Fachrichtung(models.Model):
+    kategorie = models.CharField(verbose_name=("Kategorie"), max_length=50, primary_key=True)
+    bezeichnung = models.CharField(verbose_name=("Bezeichnung"), max_length=255, blank=True)
+    kunde = models.CharField(verbose_name=("Kunde"), max_length=255, blank=True)
+    aktiv = models.BooleanField(verbose_name=("Aktiv"), default=True)
+    def __str__(self):
+        return f"{self.kategorie} - {self.bezeichnung}"
+    class Meta:
+        verbose_name_plural = "Fachrichtungen"
+        verbose_name = "Fachrichtung"
+        ordering = ['kategorie']
+
+class Kontakt(models.Model):
+    name = models.CharField(verbose_name=("Name"), max_length=250, primary_key=True)
+    mail = models.CharField(verbose_name=("E-Mail"), max_length=250, blank=True)
+    telefon = models.CharField(verbose_name=("Telefon"), max_length=50, blank=True)
+    def __str__(self):
+        return f"{self.name}"
+    class Meta:
+        verbose_name_plural = "Kontakte"
+        verbose_name = "Kontakt"
+
+class Dokument(models.Model):
+    bezeichnung = models.CharField(verbose_name=("Bezeichnung"), max_length=250)
+    def __str__(self):
+        return f"{self.bezeichnung}"
+    class Meta:
+        verbose_name_plural = "Dokumente"
+        verbose_name = "Dokument"
+
+class Organisation(models.Model):
+    bezeichnung = models.CharField(verbose_name=("Organisation"), max_length=255, primary_key=True)
+    ansprechpartner = models.ForeignKey(Kontakt, verbose_name=("Ansprechpartner"), on_delete=models.CASCADE)
+    def __str__(self):
+        return self.bezeichnung
+    class Meta:
+        verbose_name_plural = "Organisationen"
+        verbose_name = "Organisation"
+
+class Schlagwort(models.Model):
+    schlagwort = models.CharField(verbose_name=("Schlagwort"), max_length=250, unique=True)
+    def __str__(self):
+        return f"{self.schlagwort}"
+    class Meta:
+        verbose_name_plural = "Schlagwort"
+        verbose_name = "Schlagworte"
+        ordering = ['schlagwort']
+
+class Kostentraeger(models.Model):
+    kostentraeger = models.CharField(verbose_name=("Geschäftsfeld/Kostenträger"), max_length=250, unique=True)
+    anmerkung = models.CharField(verbose_name=('Anmerkung'), max_length=250, blank=True)
+    
+    def __str__(self):
+        return f"{self.kostentraeger}"
+    class Meta:
+        verbose_name_plural = "Kostenträger"
+        verbose_name = "Kostenträger"
+        ordering = ['kostentraeger']
+
+class Reha(models.Model):
+    massnahmentitel = models.TextField(verbose_name=("Maßnahmentitel"))
+    fachrichtung = models.ForeignKey(Fachrichtung, verbose_name=("Fachrrichtung"), on_delete=models.CASCADE)
+    abrechnungsart = models.CharField(verbose_name=("Abrechnungsart"), max_length=50)
+    dauer = models.CharField(verbose_name=("Dauer"), max_length=50)
+    praxisdauer = models.CharField(verbose_name=("Praxisdauer"), max_length=50)
+    abschluss = models.TextField(verbose_name=("Abschluss"),blank=True)
+    schlagrichtung = models.TextField(verbose_name=("inhalt. Schlagrichtung"))
+    schlagwort = models.ManyToManyField(Schlagwort, verbose_name=("Schlagwort"))
+    kostentraeger = models.ManyToManyField(Kostentraeger, verbose_name=("Geschäftfeld/Kostenträger"))
+    status = models.BooleanField(verbose_name=("aktiv"))
+    dokumente = models.ForeignKey(Dokument, verbose_name=("Dokumente"), on_delete=models.CASCADE)
+    verantwortlicher = models.ForeignKey(Kontakt, verbose_name=("Maßnahmenverantwortlicher"), on_delete=models.CASCADE)
+    organisation = models.ForeignKey(Organisation, verbose_name=("Organisation"), on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.massnahmentitel} - {self.organisation}"
+    class Meta:
+        verbose_name_plural = "Rehas"
+        verbose_name = "Reha"
+ 
+class Vorschlag(models.Model):
+    vorschlag = models.CharField(verbose_name=("Vorschlag"), max_length=50)
+    begruendung = models.TextField(verbose_name=("Begründung"))
+    vorschlagdatum = models.DateField(verbose_name=("vorgeschlagen am"), auto_now=False, auto_now_add=True)
+    user = models.ForeignKey(User, verbose_name=("Benutzer"), editable=False,null=True,blank=True, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.vorschlag}/{self.user} ({self.vorschlagdatum})"
+    class Meta:
+        verbose_name_plural = "Vorschläge"
+        verbose_name = "Vorschlag"
+
+class Vorschlagbearbeitung(models.Model):
+     vorschlag = models.ForeignKey(Vorschlag, verbose_name=("Vorschlag"), on_delete=models.CASCADE)
+     anmerkung = models.TextField(verbose_name=("Anmerkung"))
+     erledigt = models.BooleanField(verbose_name=("Erledigt"), default=False)
+     datum = models.DateField(verbose_name=("Datum"), auto_now=False, auto_now_add=True)
+     user = models.ForeignKey(User, verbose_name=("Benutzer"), editable=False, null=True, blank=True, on_delete=models.CASCADE)
+     def __str__(self):
+         return f"{self.vorschlag.vorschlag} - {self.anmerkung} - {self.user}/ {self.erledigt}"
+     class Meta:
+         verbose_name_plural = "V.Bearbeitungen"
+         verbose_name = "V.Bearbeitung"
+
