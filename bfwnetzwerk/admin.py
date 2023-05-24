d6b422476b9cecb70fd8f47bbf4690ccb884fe00
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-05-24 16:40:09 +0200

    tabellen angepasst

--- bfwnetzwerk/admin.py
+++ bfwnetzwerk/admin.py
@@ -3,23 +3,28 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Fachrichtung)
-admin.site.register(Kostentraeger)
+admin.site.register(Organisation)
+admin.site.register(Massnahmeart)
 admin.site.register(Schlagwort)
+admin.site.register(Abrechnungsart)
+admin.site.register(Kostentraeger)
+admin.site.register(Produkt)
 
-@admin.register(Organisation)
-class OrganisationAdmin(admin.ModelAdmin):
-   list_display = ['bezeichnung', 'partner', 'reserve']
-   @admin.display()
-   def partner(self, organisation):
-      return f"{organisation.ansprechpartner}/{organisation.ansprechpartner_mail} Telefon: {organisation.ansprechpartner_telefon}"
-   @admin.display()
-   def reserve(self, organisation):
-      return f"{organisation.ansprechpartner2}/{organisation.ansprechpartner2_mail} Telefon: {organisation.ansprechpartner2_telefon}"
+
+
+# @admin.register(Organisation)
+# class OrganisationAdmin(admin.ModelAdmin):
+#    list_display = ['bezeichnung', 'partner', 'reserve']
+#    @admin.display()
+#    def partner(self, organisation):
+#       return f"{organisation.ansprechpartner}/{organisation.ansprechpartner_mail} Telefon: {organisation.ansprechpartner_telefon}"
+#    @admin.display()
+#    def reserve(self, organisation):
+#       return f"{organisation.ansprechpartner2}/{organisation.ansprechpartner2_mail} Telefon: {organisation.ansprechpartner2_telefon}"
       
-@admin.register(Reha)
-class RehaAdmin(admin.ModelAdmin):
-   list_display = ['massnahmentitel', 'schlagrichtung', 'organisation']
-   search_fields = ['massnahmentitel', 'schlagwort__schlagwort', 'schlagrichtung']
-   list_filter = ['kostentraeger', 'organisation', 'dauer','status', 'organisation', 'schlagwort']
+# @admin.register(Reha)
+# class RehaAdmin(admin.ModelAdmin):
+#    list_display = ['massnahmentitel', 'schlagrichtung', 'organisation']
+#    search_fields = ['massnahmentitel', 'schlagwort__schlagwort', 'schlagrichtung']
+#    list_filter = ['kostentraeger', 'organisation', 'dauer','status', 'organisation', 'schlagwort']
 
--- bfwnetzwerk/models.py
+++ bfwnetzwerk/models.py
@@ -3,17 +3,15 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Fachrichtung(models.Model):
-    kategorie = models.CharField(verbose_name=("Kategorie"), max_length=50, primary_key=True)
+class Massnahmeart(models.Model):
+    art = models.CharField(verbose_name=("Kategorie"), max_length=50, primary_key=True)
     bezeichnung = models.CharField(verbose_name=("Bezeichnung"), max_length=255, blank=True)
-    kunde = models.CharField(verbose_name=("Kunde"), max_length=255, blank=True)
-    aktiv = models.BooleanField(verbose_name=("Aktiv"), default=True)
     def __str__(self):
-        return f"{self.kategorie} - {self.bezeichnung}"
+        return f"{self.art}"
     class Meta:
-        verbose_name_plural = "Fachrichtungen"
-        verbose_name = "Fachrichtung"
-        ordering = ['kategorie']
+        verbose_name_plural = "Maßnahmearten"
+        verbose_name = "Maßnahmeart"
+        ordering = ['art']
 
 
 class Organisation(models.Model):
@@ -39,56 +37,47 @@ class Schlagwort(models.Model):
         verbose_name = "Schlagworte"
         ordering = ['schlagwort']
 
-class Kostentraeger(models.Model):
-    kostentraeger = models.CharField(verbose_name=("Geschäftsfeld/Kostenträger"), max_length=250, unique=True)
+class Abrechnungsart(models.Model):
+    kunde = models.CharField(verbose_name=("Geschäftsfeld/Kostenträger"), max_length=250, unique=True)
     anmerkung = models.CharField(verbose_name=('Anmerkung'), max_length=250, blank=True)
     
     def __str__(self):
-        return f"{self.kostentraeger}"
+        return f"{self.kunde}"
     class Meta:
-        verbose_name_plural = "Kostenträger"
-        verbose_name = "Kostenträger"
-        ordering = ['kostentraeger']
+        verbose_name_plural = "Produkte / Abrechnungsart"
+        verbose_name = "Produkt / Abrechnungsart"
+        ordering = ['kunde']
 
-class Reha(models.Model):
-    massnahmentitel = models.TextField(verbose_name=("Maßnahmentitel"))
-    fachrichtung = models.ManyToManyField(Fachrichtung, verbose_name=("Produkt(e)"), blank = True)
-    abrechnungsart = models.CharField(verbose_name=("Abrechnungsart"), max_length=50)
-    dauer = models.CharField(verbose_name=("Dauer"), max_length=50)
-    praxisdauer = models.CharField(verbose_name=("Praxisdauer"), max_length=50)
-    abschluss = models.TextField(verbose_name=("Abschluss"),blank=True)
-    schlagrichtung = models.TextField(verbose_name=("inhalt. Schlagrichtung"))
-    schlagwort = models.ManyToManyField(Schlagwort, verbose_name=("Schlagwort"))
-    kostentraeger = models.ManyToManyField(Kostentraeger, verbose_name=("Geschäftfeld/Kostenträger"))
-    status = models.BooleanField(verbose_name=("aktiv"))
-    dokumente =   models.URLField(verbose_name="Dokument", max_length=200)
-    organisation = models.ForeignKey(Organisation, verbose_name=("Organisation"), on_delete=models.CASCADE)
+class Kostentraeger(models.Model):
+    kostentraeger = models.CharField(verbose_name="Kostenträger", max_length=80)
+    anmerkung = models.TextField(verbose_name="Anmerkung")
+
+    class Meta:
+        verbose_name = "Kostenträger"
+        verbose_name_plural = "Kostenträger"
+
     def __str__(self):
-        return f"{self.massnahmentitel} - {self.organisation}"
+        return self.kostentraeger
+
+    def get_absolute_url(self):
+        return reverse("kostentraeger_detail", kwargs={"pk": self.pk})
+
+class Produkt(models.Model):
+    massnahmentitel = models.TextField(verbose_name=("Maßnahmentitel"))
+    massnahmeart = models.ForeignKey(Massnahmeart, verbose_name="Maßnahmenart", on_delete=models.RESTRICT)
+    schlagrichtung = models.TextField(verbose_name="Inhaltliche Schlagrichtung")
+    schlagwoerter = models.ManyToManyField(Schlagwort, verbose_name="Schlagwörter")
+    abrechnungsart = models.ManyToManyField(Abrechnungsart, verbose_name="Produkt / Abrechnungsart")
+    kostentraeger = models.ManyToManyField(Kostentraeger, verbose_name="Kostenträger")
+    dauer = models.CharField(verbose_name="Dauer", max_length=50)
+    praxisdauer = models.CharField(verbose_name="Praxisdauer", max_length=50, blank=True)
+    status = models.BooleanField(verbose_name="Status")
+    dokument = models.URLField(verbose_name="Dokument", max_length=200)
+    organisation = models.ForeignKey(Organisation, verbose_name="Organisation", on_delete=models.RESTRICT)
+    def __str__(self):
+        return f"{self.massnahmentitel}"
     class Meta:
         verbose_name_plural = "Produkte"
         verbose_name = "Produkt"
  
-class Vorschlag(models.Model):
-    vorschlag = models.CharField(verbose_name=("Vorschlag"), max_length=50)
-    begruendung = models.TextField(verbose_name=("Begründung"))
-    vorschlagdatum = models.DateField(verbose_name=("vorgeschlagen am"), auto_now=False, auto_now_add=True)
-    user = models.ForeignKey(User, verbose_name=("Benutzer"), editable=False,null=True,blank=True, on_delete=models.CASCADE)
-    def __str__(self):
-        return f"{self.vorschlag}/{self.user} ({self.vorschlagdatum})"
-    class Meta:
-        verbose_name_plural = "Vorschläge"
-        verbose_name = "Vorschlag"
-
-class Vorschlagbearbeitung(models.Model):
-    vorschlag = models.ForeignKey(Vorschlag, verbose_name=("Vorschlag"), on_delete=models.CASCADE)
-    anmerkung = models.TextField(verbose_name=("Anmerkung"))
-    erledigt = models.BooleanField(verbose_name=("Erledigt"), default=False)
-    datum = models.DateField(verbose_name=("Datum"), auto_now=False, auto_now_add=True)
-    user = models.ForeignKey(User, verbose_name=("Benutzer"), editable=False, null=True, blank=True, on_delete=models.CASCADE)
-    def __str__(self):
-        return f"{self.vorschlag.vorschlag} - {self.anmerkung} - {self.user}/ {self.erledigt}"
-    class Meta:
-        verbose_name_plural = "V.Bearbeitungen"
-        verbose_name = "V.Bearbeitung"
 
